bf6fc292f02dc2b8b34ab82ca45d810ce64732de
sunilmanickalal <sunil.manickalal@hotmail.com>
2017-03-23 00:49:19 -0400

    Removed old person rest service

--- personRestService/src/main/java/com/sunil/sources/repositories/PersonRepository.java
+++
@@ -1,9 +0,0 @@
-package com.sunil.sources.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.sunil.sources.entities.Person;
-
-public interface PersonRepository extends CrudRepository<Person, Integer> {
-
-}
